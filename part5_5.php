33b9d1dcaf6e588640262ee21bdd4da45cc2925d
tom--bo <otsuka.t.2013@gmail.com>
2014-07-22 02:03:41 +0900

    クラスとオブジェクト終了

--- part5_5.php
+++ part5_5.php
@@ -1,3 +1,26 @@
 <?php
 // 例外
+class ZeroDivisionException extends Exception
+{
 
+}
+
+function div ($v1, $v2)
+{
+    if($v2 === 0){
+        throw new ZeroDivisionException("arg #2 is zero.");
+    }
+    return $v1/$v2;
+}
+
+try{
+    echo div(1,2)."<br>";
+    echo div(1,0)."<br>";
+    echo div(2,1)."<br>";
+}catch(ZeroDivisionException $e) {
+    echo 'Zero Division Exception!<br>';
+    echo $e->getMessage().'<br>';
+}catch(Exception $e){
+    echo 'Exception!'.'<br>';
+    echo $e->getMessage().'<br>';
+}
--- part5_6.php
+++ part5_6.php
@@ -1,3 +1,23 @@
 <?php
 // phpの参照
+function array_pass($array)
+{
+    $array[0] *=2;
+    $array[1] *=2;
+}
 
+function array_pass_ref(&$array)
+{
+    $array[0] *= 2;
+    $array[1] *= 2;
+}
+
+$a = 10;
+$b = 20;
+
+$array = array($a, &$b);
+// array_pass($array);
+array_pass_ref($array);
+
+echo $a.'<br>';
+echo $b.'<br>';
